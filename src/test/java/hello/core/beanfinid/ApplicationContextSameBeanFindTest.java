61b1f47a80ca0a7df566ccdf03edf7312d0f4e67
JG HAN <gojkhan13@gmail.com>
2021-05-08 15:21:44 +0900

    0508 스프링컨테이너에 등록된 Bean조회4 - 동일한 타입 조회

--- src/test/java/hello/core/beanfinid/ApplicationContextSameBeanFindTest.java
+++ src/test/java/hello/core/beanfinid/ApplicationContextSameBeanFindTest.java
@@ -13,6 +13,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 
@@ -22,31 +24,42 @@ public class ApplicationContextSameBeanFindTest {
 
     @Test
     @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 중복 오류가 발생한다.")
-    void findBeanByTypeDuplicate(){
+    void findBeanByTypeDuplicate() {
         MemberRepository bean = ac.getBean(MemberRepository.class);
         assertThrows(NoUniqueBeanDefinitionException.class,
-                ()->  ac.getBean(MemberRepository.class));
+                () -> ac.getBean(MemberRepository.class));
     }
 
     @Test
     @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 빈 이름을 지정하면 된다.")
-    void findBeanByName(){
-        MemberRepository memberRepository = ac.getBean(MemberRepository.class);
+    void findBeanByName() {
+        MemberRepository memberRepository = ac.getBean("memberRepository1", MemberRepository.class);
         assertThat(memberRepository).isInstanceOf(MemberRepository.class);
+    }
 
+    @Test
+    @DisplayName("특정타입을 모두 조회하기")
+    void findAllBeanByType(){
+        Map<String, MemberRepository> beansOfType = ac.getBeansOfType(MemberRepository.class); //맵(키/벨류)로 나온다
+        for(String key: beansOfType.keySet()){
+            System.out.println("key = " + key + " value= " + beansOfType.get(key));
+        }
+        System.out.println("beansOfType = " + beansOfType);
+        assertThat(beansOfType.size()).isEqualTo(2);
     }
 
 
     @Configuration
-    static class SameBeanConfig{
+
+    static class SameBeanConfig {
 
         @Bean
-        public MemberRepository memberRepository1(){
+        public MemberRepository memberRepository1() {
             return new MemoryMemberRepository();
         }
 
         @Bean
-        public MemberRepository memberRepository2(){
+        public MemberRepository memberRepository2() {
             return new MemoryMemberRepository();
         }
     }
