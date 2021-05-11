0877f01655a547123dd69f493ca252fb2254734d
JG HAN <gojkhan13@gmail.com>
2021-05-11 23:52:50 +0900

    0511 @Componentcan, @Autowired 1

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,12 +1,18 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 //구현체가 하나만 있을때 인터페이스명 뒤에 Impl 붙이는게 관례!
+@Component
 public class MemberServiceImpl implements MemberService{
 
     //가입하고 찾으려면 MemberRepository 구현객체가 필요
     //이제 추상화에만 의존 DIP지킴
     private final MemberRepository memberRepository;
 
+    //자동의존관계주입 - 생성자에 붙여주면 스프링이 MemberRepository타입에 맞는 애를 의존관계 주입을 해준다
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 //메모리에서 저장, 찾기 - 테스트용
+@Component
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
