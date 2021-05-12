4d1e68bfa7554b1f586d7e1b21cdc4bda3f17e00
JG HAN <gojkhan13@gmail.com>
2021-05-12 22:02:00 +0900

    0512 탐색위치

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core", //컴포넌트 스캔의 대상
+        //만약 지정안하면 AutoAppConfig의 클래스의 패키지의 모든 하위 클래스를 스캔한다
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes =  Configuration.class)
 )
 public class AutoAppConfig {
